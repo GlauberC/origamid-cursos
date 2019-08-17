f85c94503f28398451bde2ae91d5c7b804e109d2
glauberc <glaubercarv@gmail.com>
2019-08-17 16:27:45 -0300

    flexbox

--- flexbox/src/components/newsletter.js
+++ flexbox/src/components/newsletter.js
@@ -1,15 +1,19 @@
 import React from "react";
+import "../styles/newsletter.scss";
+
 export default () => {
   return (
     <section className="newsletter">
-      <div className="newsletter-info">
-        <h1>Newsletter</h1>
-        <p>assin e fique por dentro das novidades</p>
+      <div className="newsletter-container">
+        <div className="newsletter-info">
+          <h1>Newsletter</h1>
+          <p>assine e fique por dentro das novidades</p>
+        </div>
+        <form className="newsletter-form">
+          <input type="text" placeholder="Seu e-mail" />
+          <button type="submit">Assinar</button>
+        </form>
       </div>
-      <form action="" className="newsletter-form">
-        <input type="text" placeholder="Seu e-mail" />
-        <button type="submit">Assinar</button>
-      </form>
     </section>
   );
 };
--- flexbox/src/components/qualidade.js
+++ flexbox/src/components/qualidade.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "../styles/qualidade.scss";
+
 export default () => {
   return (
     <section className="qualidade" id="qualidade">
